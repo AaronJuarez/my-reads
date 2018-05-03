4691ca667595a6c50c68f2acf5c500627de8bce4
Aaron Juarez <aarjuamtz@gmail.com>
2018-05-02 21:37:14 -0500

    feat: book shelf state

--- src/App.js
+++ src/App.js
@@ -7,20 +7,41 @@ import BooksShelf from './BooksShelf'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+    books: []
+  }
+
+  updateBoook = (book, newShelf) => {
+    this.setState((currentState) => ({
+      books: currentState.books.map((b) => {
+        if (b.id === book.id) {
+          b.shelf = newShelf
+          BooksAPI.update(book, newShelf)
+        }
+        return b
+      })
+    }))
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+      .then((books) => {
+        this.setState(() => ({
+          books: books
+        }))
+        console.log(this.state);
+      })
   }
 
   render() {
+    console.log(this.state);
     return (
       <div className="app">
 
         <Route exact path='/' render={() => (
-          <BooksShelf />
+          <BooksShelf
+            books={this.state.books}
+            onShelfUpdate={this.updateBoook}
+          />
         )} />
 
         <Route path='/search' render={() => (
